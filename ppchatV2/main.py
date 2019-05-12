720a00fcffa186030d0f89970ff339de80f6006e
libochen7 <libochen7@gmail.com>
2019-05-12 20:46:34 +0800

    main frame

--- ppchatV2/main.py
+++ ppchatV2/main.py
@@ -1,24 +1,14 @@
-from flask import Flask, session, redirect, url_for, escape, request
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
-    return 'You are not logged in'
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     if request.method == 'POST':
         session['username'] = request.form['username']
-        return redirect(url_for('index'))
-    return '''
-        <form action="" method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
+        return redirect(url_for("index"))
+    html = render_template('main.html', pagetitle="主页" )
+    return html
 
 @app.route('/logout')
 def logout():
@@ -26,8 +16,18 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+@app.route('/idgen')
+def idgen():
+    html = render_template('main.html', pagetitle="身份生成" )
+    return html
+
+@app.route('/chatroom')
+def chatroom():
+    html = render_template('main.html', pagetitle="聊天" )
+    return html
+
 # set the secret key.  keep this really secret:
 app.secret_key = b'b\xd1\x10#\xe3\xca\xfd\\A\x10\xffh\xff\xbc\x92\x10<\x92\x11G[*\xa5['
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='192.168.0.107')

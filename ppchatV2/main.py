4ea16eb77e256d6405eeb65ce7053e9e1c609b5e
libochen7 <libochen7@gmail.com>
2019-05-12 22:53:10 +0800

    add idgen

--- ppchatV2/main.py
+++ ppchatV2/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template
+from idgen import gen_once 
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ def index():
     if request.method == 'POST':
         session['username'] = request.form['username']
         return redirect(url_for("index"))
-    html = render_template('main.html', pagetitle="主页" )
+    html = render_template('main.html.j2' )
     return html
 
 @app.route('/logout')
@@ -16,18 +17,27 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
-@app.route('/idgen')
+@app.route('/idgen', methods=['GET', 'POST'])
 def idgen():
-    html = render_template('main.html', pagetitle="身份生成" )
+    if request.method == 'POST':
+        region_code = request.form.get('region_code')
+        region_code = int(region_code) if region_code else None
+        date_code = request.form.get('date_code')
+        date_code = int(date_code) if date_code else None
+        prcid = gen_once(region_code, date_code)
+        html = render_template('idgen.html.j2', prcid=prcid)
+    else:
+        html = render_template('idgen.html.j2')
     return html
 
 @app.route('/chatroom')
 def chatroom():
-    html = render_template('main.html', pagetitle="聊天" )
+    html = render_template('main.html.j2')
     return html
 
 # set the secret key.  keep this really secret:
 app.secret_key = b'b\xd1\x10#\xe3\xca\xfd\\A\x10\xffh\xff\xbc\x92\x10<\x92\x11G[*\xa5['
+app.permanent_session_lifetime = 3600*24*7
 
 if __name__ == "__main__":
     app.run(host='192.168.0.107')
